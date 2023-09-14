47da3fd85e69e7a3e9d310a30b9f8664dce6b715
DikDns <dikdnssocial@gmail.com>
2023-09-14 15:19:17 +0700

    add test.py

---
+++ pertemuan-2/test.py
@@ -0,0 +1,32 @@
+
+
+# # Ini adalah program Gaje
+
+# #! Ini memasukan string dari user
+# string = input("Masukkan string: ")
+# # ? Ini memasukan nilai n dari user
+# n = int(input("Masukkan n: "))
+
+# """
+#   Looping
+
+# """
+# index = 0
+# while index < n:
+#     print(string * index)
+#     index += 1
+
+
+# x = 2
+# y = 6
+
+# print(str(x + y) + "5" * 4)
+# print(type(str(x + y) + "5" * 4))
+
+
+# a = 10
+# b = 20
+# print(f"Nilai a adalah {a} dan b adalah {b}")
+
+
+# print(0.11111111111111111111111111111111111111111111111111111111111111111111111111111119)
