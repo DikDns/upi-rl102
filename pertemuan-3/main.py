74377bce937b81e0dc85361c893311a850fdd071
DikDns <dikdnssocial@gmail.com>
2023-09-21 14:15:10 +0700

    [pertemuan-3] add: main.py

---
+++ pertemuan-3/main.py
@@ -0,0 +1,48 @@
+"""
+  Pertemuan 3
+
+  Data Type: List and Tuple 
+"""
+
+# List
+buah = ["apel", "jeruk", "mangga", "anggur", "pisang", "semangka", "apel"]
+
+# mengambil data
+print("\n[]============| Mengambil Data |============[]")
+print(buah[3])
+print(buah[4:])
+print(buah[:4])
+print(buah[1:4])
+
+# menambah data
+print("\n[]============| Menambah Data    |============[]")
+buah.append("durian")
+print(buah[-1])
+
+buah.insert(2, "nanas")
+print(buah[2])
+
+# menghapus data
+print("\n[]============| Menghapus Data   |============[]")
+buah.remove("apel")
+buah.pop(2)
+print(buah)
+
+
+# mengurutkan data
+print("\n[]============| Mengurutkan Data |============[]")
+buah.sort()
+print(buah)
+
+# Tuple
+makanan_tradisional = ("rendang", "sate", "soto",
+                       "nasi goreng", "bakso", "rendang")
+
+# Unpacking
+print("\n[]============| Unpacking Data   |============[]")
+makanan1, makanan2, makanan3, *makanan_lain = makanan_tradisional
+
+print(makanan1)
+print(makanan2)
+print(makanan3)
+print(makanan_lain)
