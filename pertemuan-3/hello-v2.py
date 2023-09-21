c299539c2e79c559757873a9f2b06c083bd60fa9
DikDns <dikdnssocial@gmail.com>
2023-09-21 13:32:43 +0700

    [pertemuan-3] add: tahun lahir input in hello-v2

---
+++ pertemuan-3/hello-v2.py
@@ -0,0 +1,7 @@
+
+nama = input("Masukkan nama: ")
+tahun_sekarang = 2023
+tahun_lahir = int(input("Masukkan tahun lahir anda: "))
+
+print(
+    f"Selamat datang, {nama}! Umur anda sekarang adalah {tahun_sekarang - tahun_lahir}")
