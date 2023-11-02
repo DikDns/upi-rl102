1cc60e318c33b3dd4eae2810aa1bc5162f32362f
DikDns <dikdnssocial@gmail.com>
2023-11-02 07:35:54 +0700

    delete latihan-uts folder

--- latihan-uts/no-1.py
+++
@@ -1,3 +0,0 @@
-
-celcius = int(input("Masukkan suhu dalam Celcius: "))
-print(f"Suhu {celcius} derajat celcius = {9/5 * celcius + 32} derajat Fahrenheit")
--- latihan-uts/no-2.py
+++
@@ -1,7 +0,0 @@
-n = int(input("Masukkan angka: "))
-
-jumlah = 0
-for angka in range(3, n, 3):
-    jumlah += angka
-
-print(f"Jumlah bilangan kelipatan 3 dibawah {n} adalah {jumlah}")
